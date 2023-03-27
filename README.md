ecb2daa1d828ebbd9a0c08f02bd5551969e46e31
kingyo <kingyo.no.esa1192@gmail.com>
2023-03-28 00:26:31 +0900

    Add some files..

--- README.md
+++ README.md
@@ -4,11 +4,21 @@ RJ45 connector for Raspberry Pi Pico.
 10BASE-T communication using Raspberry Pi Pico's PIO and software stack.  
 See also : [Pico-10BASE-T](https://github.com/kingyoPiyo/Pico-10BASE-T "Pico-10BASE-T")
 
+## Board Layout
 <img src="img/board_f.png" width="500">  
 <img src="img/board_b.png" width="500">  
 
+## Assembly
+<img src="img/assembly.jpg" width="500">  
+
 ## Schematic
 [pico_rj45.pdf](kicad/pico_rj45.pdf "Schematic")
+
+## Sample Firmware
+| Link | Description |
+| ---- | ---- |
+| [Hello World](firmware/hello_world/ "hello_world") | ARP, UDP, ICMP Test.<br>Change *system.h* for connection settings.|
+
 
 ## Reference
 * Pico DVI Sock https://github.com/Wren6991/Pico-DVI-Sock
---
+++ firmware/hello_world/main.c
@@ -0,0 +1,48 @@
+/********************************************************
+* Title    : Pico-10BASE-T Sample
+* Date     : 2022/08/22
+* Design   : kingyo
+********************************************************/
+#include "pico/stdlib.h"
+#include "hardware/vreg.h"
+#include "hardware/irq.h"
+#include "eth.h"
+
+#define HW_PINNUM_LED0      (25)    // Pico onboard LED
+
+static struct repeating_timer timer;
+
+
+// Timer interrupt (L-tika)
+static bool repeating_timer_callback(struct repeating_timer *t) {
+    static bool led0_state = true;
+
+    gpio_put(HW_PINNUM_LED0, led0_state);
+    led0_state = !led0_state;
+
+    return true;
+}
+
+
+int main() {
+    // Core voltage
+    //vreg_set_voltage(VREG_VOLTAGE_1_10); // default
+    vreg_set_voltage(VREG_VOLTAGE_1_20); // Increase core voltage to stabilize overclocking
+    sleep_ms(10);
+    set_sys_clock_khz(240000, true);    // Over clock 240MHz
+
+    stdio_init_all();
+    eth_init();
+
+    printf("[BOOT]\r\n");
+
+    // Onboard LED tikatika~
+    gpio_init(HW_PINNUM_LED0);
+    gpio_set_dir(HW_PINNUM_LED0, GPIO_OUT);
+    add_repeating_timer_ms(-500, repeating_timer_callback, NULL, &timer);
+
+    while (1) {
+        eth_main();
+    }
+
+}
