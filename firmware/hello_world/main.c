f6e52fd01b64a17be385af99f3b166ebf0132408
kingyo <kingyo.no.esa1192@gmail.com>
2023-12-28 10:15:53 +0900

    Update

--- firmware/hello_world/main.c
+++ firmware/hello_world/main.c
@@ -5,45 +5,20 @@
 ********************************************************/
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/vreg.h"
-#include "hardware/irq.h"
+#include "hwinit.h"
 #include "eth.h"
-
-#define HW_PINNUM_LED0      (25)    // Pico onboard LED
-
-static struct repeating_timer timer;
-
-
-// Timer interrupt (L-tika)
-static bool repeating_timer_callback(struct repeating_timer *t) {
-    static bool led0_state = true;
-
-    gpio_put(HW_PINNUM_LED0, led0_state);
-    led0_state = !led0_state;
-
-    return true;
-}
 
 
 int main() {
-    // Core voltage
-    //vreg_set_voltage(VREG_VOLTAGE_1_10); // default
-    vreg_set_voltage(VREG_VOLTAGE_1_20);    // Increase core voltage to stabilize overclocking
-    sleep_ms(10);
-    set_sys_clock_khz(240000, true);        // Over clock 240MHz
-
     stdio_init_all();
+    hw_init();
     eth_init();
 
     printf("[BOOT]\r\n");
 
-    // Onboard LED tikatika~
-    gpio_init(HW_PINNUM_LED0);
-    gpio_set_dir(HW_PINNUM_LED0, GPIO_OUT);
-    add_repeating_timer_ms(-500, repeating_timer_callback, NULL, &timer);
+    hw_start_led_blink();
 
     while (1) {
         eth_main();
     }
-
 }
